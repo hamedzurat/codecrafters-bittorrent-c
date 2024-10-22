08048ed76f94415c8954bf1b6079c691ac9012e9
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-10-22 21:26:08 +0600

    format and understand current code

--- app/main.c
+++ app/main.c
@@ -1,21 +1,23 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 bool is_digit(char c) {
     return c >= '0' && c <= '9';
 }
 
 char* decode_bencode(const char* bencoded_value) {
-    if (is_digit(bencoded_value[0])) {
-        int length = atoi(bencoded_value);
-        const char* colon_index = strchr(bencoded_value, ':');
-        if (colon_index != NULL) {
-            const char* start = colon_index + 1;
-            char* decoded_str = (char*)malloc(length + 1);
-            strncpy(decoded_str, start, length);
-            decoded_str[length] = '\0';
+    if(is_digit(bencoded_value[0])) {
+        int length              = atoi(bencoded_value);         // decodes string to integer as long as possible
+        const char* colon_index = strchr(bencoded_value, ':');  // finds the index of :
+
+        if(colon_index != NULL) {
+            const char* start = colon_index + 1;            // skip :
+            char* decoded_str = (char*)malloc(length + 1);  // as lenght is string lenght, it knows how much to allocate
+            strncpy(decoded_str, start, length);            // copies
+            decoded_str[length] = '\0';                     // null termination
+
             return decoded_str;
         } else {
             fprintf(stderr, "Invalid encoded value: %s\n", bencoded_value);
@@ -28,26 +30,26 @@ char* decode_bencode(const char* bencoded_value) {
 }
 
 int main(int argc, char* argv[]) {
-	// Disable output buffering
-	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+    // Disable output buffering
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
 
-    if (argc < 3) {
+    // help menu
+    if(argc < 3) {
         fprintf(stderr, "Usage: your_bittorrent.sh <command> <args>\n");
         return 1;
     }
 
     const char* command = argv[1];
 
-    if (strcmp(command, "decode") == 0) {
-    	// You can use print statements as follows for debugging, they'll be visible when running tests.
-        // printf("Logs from your program will appear here!\n");
-            
-        // Uncomment this block to pass the first stage
+    // switch
+    if(strcmp(command, "decode") == 0) {
         const char* encoded_str = argv[2];
+
         char* decoded_str = decode_bencode(encoded_str);
         printf("\"%s\"\n", decoded_str);
-        free(decoded_str);
+
+        free(decoded_str);  // free memory
     } else {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
