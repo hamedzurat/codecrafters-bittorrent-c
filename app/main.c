df6f1153fe7c8b7c5a8ed608226898d1da2cab96
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-10-25 19:48:06 +0600

    adding command to do handshake with other peers

--- app/main.c
+++ app/main.c
@@ -1,9 +1,11 @@
+#include <arpa/inet.h>
 #include <curl/curl.h>
 #include <openssl/sha.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct {
     char* announce;
@@ -23,6 +25,7 @@ typedef struct {
     int complete;
     int incomplete;
     unsigned char* peers;
+    int peer_numer;
 } TrackerRes;
 
 bool is_digit(char c) {
@@ -198,32 +201,78 @@ char* open_file(char* filename) {
 size_t callback(void* ptr, size_t size, size_t nmemb, void* stream) {
     int index         = 0;
     char* decoded_str = decode_bencode(ptr, &index);  // decode
+    TrackerRes* tres  = (TrackerRes*)stream;
 
-    TrackerRes tres;
-    tres.interval     = extract_int(ptr, "8:interval");
-    tres.min_interval = extract_int(ptr, "12:min interval");
-    tres.complete     = extract_int(ptr, "8:complete");
-    tres.incomplete   = extract_int(ptr, "10:incomplete");
+    tres->interval     = extract_int(ptr, "8:interval");
+    tres->min_interval = extract_int(ptr, "12:min interval");
+    tres->complete     = extract_int(ptr, "8:complete");
+    tres->incomplete   = extract_int(ptr, "10:incomplete");
 
     char* peers_val_start = strstr(ptr, "5:peers") + strlen("5:peers");
     char* peers_start     = strstr(peers_val_start, ":") + 1;
-    int peer_numer        = atoi(peers_val_start);
-    tres.peers            = malloc(peer_numer);
-    memcpy(tres.peers, peers_start, peer_numer);
-
-    for(size_t i = 0; i < peer_numer; i += 6) {
-        unsigned char ip1 = tres.peers[i + 0];
-        unsigned char ip2 = tres.peers[i + 1];
-        unsigned char ip3 = tres.peers[i + 2];
-        unsigned char ip4 = tres.peers[i + 3];
-        uint16_t port     = (tres.peers[i + 4] << 8) | tres.peers[i + 5];  // 2 bytes for port
-
-        printf("%d.%d.%d.%d:%d\n", ip1, ip2, ip3, ip4, port);
-    }
+    tres->peer_numer      = atoi(peers_val_start);
+    tres->peers           = malloc(tres->peer_numer);
+    memcpy(tres->peers, peers_start, tres->peer_numer);
 
     free(decoded_str);
-    free(tres.peers);
     return size * nmemb;
+}
+
+TrackerRes call_tracker(TorrentInfo torrent) {
+    CURL* curl;
+    CURLcode res;
+    TrackerRes tres;
+
+    char* announce = torrent.announce;
+    char* infohash = curl_easy_escape(NULL, torrent.infohash, 20);
+    char* peer_id  = curl_easy_escape(NULL, "-PC0001-123456789012", 20);
+    int port       = 6881;
+    int uploaded   = 0;
+    int downloaded = 0;
+    int left       = torrent.info.length;
+    int compact    = 1;
+
+    char url[512];
+    snprintf(url, sizeof(url), "%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&compact=%d", announce, infohash, peer_id, port, uploaded, downloaded, left, compact);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&tres);
+
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK) fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+    return tres;
+}
+
+unsigned char* byte_to_ip(const unsigned char* peers) {
+    static unsigned char ip[22];
+
+    unsigned char ip1 = peers[0];
+    unsigned char ip2 = peers[1];
+    unsigned char ip3 = peers[2];
+    unsigned char ip4 = peers[3];
+    uint16_t port     = (peers[4] << 8) | peers[5];  // 2 bytes for port
+
+    snprintf((char*)ip, sizeof(ip), "%d.%d.%d.%d:%d", ip1, ip2, ip3, ip4, port);
+
+    return ip;
+}
+
+void hex(const char* header, const unsigned char* src, const int size) {
+    printf("%s: ", header);
+    for(size_t i = 0; i < size; i++) {
+        printf("%02x", src[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -279,35 +328,126 @@ int main(int argc, char* argv[]) {
         char* decoded_str   = decode_bencode(encoded_str, &index);  // decode
         TorrentInfo torrent = decode_torrent(encoded_str, decoded_str);
 
-        CURL* curl;
-        CURLcode res;
+        TrackerRes tres = call_tracker(torrent);
 
-        char* announce = torrent.announce;
-        char* infohash = curl_easy_escape(NULL, torrent.infohash, 20);
-        char* peer_id  = curl_easy_escape(NULL, "-PC0001-123456789012", 20);
-        int port       = 6881;
-        int uploaded   = 0;
-        int downloaded = 0;
-        int left       = torrent.info.length;
-        int compact    = 1;
-
-        char url[512];
-        snprintf(url, sizeof(url), "%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&compact=%d", announce, infohash, peer_id, port, uploaded, downloaded, left, compact);
-
-        curl_global_init(CURL_GLOBAL_DEFAULT);
-        curl = curl_easy_init();
-        if(curl) {
-            curl_easy_setopt(curl, CURLOPT_URL, url);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-            res = curl_easy_perform(curl);
-
-            if(res != CURLE_OK) fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
-            curl_easy_cleanup(curl);
+        for(size_t i = 0; i < tres.peer_numer; i += 6) {
+            printf("%s\n", byte_to_ip(tres.peers + i));
         }
 
-        curl_global_cleanup();
+        free(torrent.announce);
+        free(torrent.info.name);
+        free(torrent.info.pieces);
+        free(tres.peers);
+        free(decoded_str);
+    } else if(strcmp(command, "handshake") == 0) {
+        encoded_str         = open_file(argv[2]);
+        char* decoded_str   = decode_bencode(encoded_str, &index);  // decode
+        TorrentInfo torrent = decode_torrent(encoded_str, decoded_str);
 
+        unsigned char* ip_port = argv[3];
+
+        if(argc < 4) {
+            TrackerRes tres = call_tracker(torrent);
+            ip_port         = byte_to_ip(tres.peers);
+            free(tres.peers);
+        }
+
+        if(!ip_port) {
+            fprintf(stderr, "No <ip>:<port>\n");
+            exit(1);
+        }
+
+        unsigned char* colon_pos = strstr(ip_port, ":");
+        if(!colon_pos) {
+            fprintf(stderr, "Invalid format. Expected <peer_ip>:<peer_port>\n");
+            exit(1);
+        }
+
+        size_t ip_len = colon_pos - ip_port;
+        char ip[16];  // Buffer for the IP address
+        strncpy(ip, ip_port, ip_len);
+        ip[ip_len] = '\0';
+
+        int port = atoi(colon_pos + 1);
+
+        // printf("IP: %s\n", ip);
+        // printf("Port: %d\n", port);
+
+        unsigned char handshake[48 + SHA_DIGEST_LENGTH];
+        size_t offset = 0;
+
+        // 1. length of the protocol string (BitTorrent protocol) which is 19 (1 byte)
+        handshake[offset] = 19;
+        offset++;
+        // 2. the string BitTorrent protocol (19 bytes)
+        memcpy(handshake + offset, "BitTorrent protocol", 19);
+        offset += 19;
+        // 3. eight reserved bytes, which are all set to zero (8 bytes)
+        memset(handshake + offset, 0, 8);
+        offset += 8;
+        // 4. sha1 infohash (20 bytes) (NOT the hexadecimal representation, which is 40 bytes long)
+        memcpy(handshake + offset, torrent.infohash, SHA_DIGEST_LENGTH);
+        offset += SHA_DIGEST_LENGTH;
+        // 5. peer id (20 bytes) (generate 20 random byte values)
+        memcpy(handshake + offset, "-PC0001-123456789012", 20);
+
+        // hex("Handshake Message", handshake, sizeof(handshake));
+
+        int sockfd;
+        struct sockaddr_in server_addr;
+
+        // Create TCP socket
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd < 0) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Set server information
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port   = htons(port);
+        if(inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+            perror("Invalid address or address not supported");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Connect to the server
+        if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            perror("Connection failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Send binary data
+        ssize_t sent_bytes = send(sockfd, handshake, sizeof(handshake), 0);
+        if(sent_bytes < 0) {
+            perror("Failed to send data");
+        } else {
+            // printf("Sent %ld bytes of binary data\n", sent_bytes);
+        }
+
+        // Buffer to receive the handshake response
+        unsigned char response[48 + SHA_DIGEST_LENGTH];
+        ssize_t received_bytes = recv(sockfd, response, sizeof(response), 0);
+        if(received_bytes < 0) {
+            perror("Failed to receive data");
+        } else {
+            // printf("Received %ld bytes of binary data\n", received_bytes);
+
+            // Check if we received a complete handshake
+            if(received_bytes == sizeof(response)) {
+                unsigned char peer_id[20];
+                memcpy(peer_id, response + 48, 20);  // peerid starts 48
+
+                hex("Peer ID", peer_id, sizeof(peer_id));  // print peerid
+            } else {
+                printf("Received incomplete handshake\n");
+            }
+        }
+        // hex("Response Message", response, sizeof(response));
+
+        close(sockfd);
         free(torrent.announce);
         free(torrent.info.name);
         free(torrent.info.pieces);
